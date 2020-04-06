1e0d093fb4c0a7dce3a040d8afde68f92fae63fa
superfrost <otkycu@gmail.com>
2020-04-06 17:38:22 +0300

    add some stufs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,66 @@
 import React from 'react';
+import { useEffect } from 'react'
 import TodoList from './Todo/TodoList'
+import Context from './contex'
+import AddTodo from './Todo/AddTodo';
+import Loader from './Loader'
 
 function App() {
-  const todos = [
-    {id: 1, completed: false, title: 'Buy bread'},
-    {id: 2, completed: false, title: 'Buy butter'},
-    {id: 3, completed: false, title: 'Buy milk'}
-  ]
-  
-  return (
-    <div className="wrapper">
-      <h1>React tutorial</h1>
+  const [todos, setTodos] = React.useState([])
+  const [loading, setLoading] = React.useState(true)
 
-      <TodoList todos={todos}></TodoList>
-    </div>
-  );
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTimeout(() => {
+          setTodos(todos)
+          setLoading(false)
+        }, 2000)
+      })
+  }, [])
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    )
+  }
+  
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(
+      todos.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: false
+        }
+      ])
+    )
+  }
+
+  return (
+    <Context.Provider value={{removeTodo: removeTodo}}>
+      <div className="wrapper">
+        <h1>React tutorial</h1>
+        <AddTodo onCreate={addTodo}></AddTodo>
+        {loading && <Loader />}
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggleTodo}/>
+         ) : (
+          loading ? null : <p>No todos!</p>
+         )}
+      </div>  
+    </Context.Provider>
+  )
 }
 
 export default App;
--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,18 +1,54 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Context from '../contex'
+import { useContext } from 'react'
 
-function TodoItem({ todo, index }) {
+const styles = {
+    li: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alingItems: 'center',
+        padding: '.5rem 1rem',
+        border: '1px solid #ccc',
+        borderRadius: '4px',
+        marginBottom: '.5rem' 
+    },
+    input: {
+        marginRight: '1rem'
+    }
+    
+}
+
+function TodoItem({ todo, index, onChange }) {
+    const { removeTodo } = useContext(Context)
+    const classes = []
+
+    if (todo.completed) {
+        classes.push('done')
+    }
     return (
-        <li>
-            <strong>{index + 1}</strong>
-            {todo.title}
+        <li style={styles.li}>
+            <span className={classes.join('')}>
+                <input 
+                    type="checkbox"
+                    checked={todo.completed}
+                    style={styles.input} 
+                    onChange={() => onChange(todo.id)}
+                />
+                <strong>{index + 1}</strong>
+                &nbsp;
+                {todo.title}
+            </span>
+
+            <button className="rm" onClick={removeTodo.bind(null, todo.id)}>&times;</button>
         </li>
     )
 }
 
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired,
-    index: PropTypes.number
+    index: PropTypes.number,
+    onChange: PropTypes.func.isRequired
 }
 
 export default TodoItem
