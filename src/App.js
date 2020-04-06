e2fc0b7c61bb577ffb5c9e99df1ca7634e54d044
superfrost <otkycu@gmail.com>
2020-04-06 11:49:49 +0300

    add prop-types and some styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoList from './Todo/TodoList'
 
 function App() {
+  const todos = [
+    {id: 1, completed: false, title: 'Buy bread'},
+    {id: 2, completed: false, title: 'Buy butter'},
+    {id: 3, completed: false, title: 'Buy milk'}
+  ]
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>React tutorial</h1>
+
+      <TodoList todos={todos}></TodoList>
     </div>
   );
 }
